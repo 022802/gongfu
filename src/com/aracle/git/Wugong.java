6e75ba00cf139a238a3c3d7b116ace713cef0a6b
suoxuewu <1253754259@qq.com>
2018-06-13 21:23:31 +0800

    更新2

--- src/com/aracle/git/Wugong.java
+++ src/com/aracle/git/Wugong.java
@@ -3,5 +3,7 @@ package com.aracle.git;
 public class Wugong {
 	public static void main(String[] args) {
 		System.out.println("11111111111");
+		System.out.println("222222222222");
+		System.out.println("11111111111");
 	}
 }
