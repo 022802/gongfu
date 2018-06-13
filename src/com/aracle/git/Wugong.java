6f8858ca8083a1713785a550205290f2073fbd0f
suoxuewu <1253754259@qq.com>
2018-06-13 22:35:28 +0800

    ss

--- src/com/aracle/git/Wugong.java
+++ src/com/aracle/git/Wugong.java
@@ -4,11 +4,6 @@ public class Wugong {
 	public static void main(String[] args) {
 		System.out.println("11111111111");
 		System.out.println("11111111111");
-		System.out.println("11111111111");
-		System.out.println("11111111111");
-		System.out.println("11111111111");
-		System.out.println("11111111111");
-		System.out.println("11111111111");
-		System.out.println("11111111111");
+	
 	}
 }
